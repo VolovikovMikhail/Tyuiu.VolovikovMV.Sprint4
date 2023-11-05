aea92f9d931f453885cc8e7c2e780e2d731d5678
VolovikovMikhail <volovikov.1995@bk.ru>
2023-11-05 19:19:11 +0500

    Task1

---
+++ Tyuiu.VolovikovMV.Sprint4.Task1.V17/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolovikovMV.Sprint4.Task1.V17.Lib;
+
+namespace Tyuiu.VolovikovMV.Sprint4.Task1.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 0;
+
+            Console.Title = "Спринт #4 | Выполнила: Воловиков М.В. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы.(ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнила: Воловиков Михаил Васильевич | АСОиУб-23-3                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 14 элементов заполненный         *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 0 до 7 подсчитать сумму          *");
+            Console.WriteLine("* четных элементов массива.                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int len;
+            Console.WriteLine("Введите количество элементов массива: ");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[len];
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("Введите значение " + i + " элемента массива: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма четных элементов массива: ");
+
+            int sumArray = ds.Calculate(array);
+
+            Console.WriteLine(sumArray);
+
+            Console.ReadKey();
+        }
+    }
+}
