114a429babb468860375f2b5cdcb90e197e0c142
VolovikovMikhail <volovikov.1995@bk.ru>
2023-11-05 19:00:59 +0500

    Add project files.

---
+++ Tyuiu.VolovikovMV.Sprint4.Task0.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VolovikovMV.Sprint4.Task0.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VolovikovMV.Sprint4.Task0.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5001e75e-0c79-4bd0-8366-8d5319ef9afd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.VolovikovMV.Sprint4.Task0.V15/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolovikovMV.Sprint4.Task0.V15.Lib;
+
+namespace Tyuiu.VolovikovMV.Sprint4.Task0.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 1;
+
+            Console.Title = "Спринт #4 | Выполнила: Воловиков М.В. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы.(статический ввод)                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнила: Воловиков Михаил Васильевич | АСОиУб-23-3                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать произведение   *");
+            Console.WriteLine("* нечетных элементов массива.                                             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] array = { 9, 8, 7, 6, 5, 7, 3, 2, 7, 3 };
+
+            Console.WriteLine(" Исходный массив: ");
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение нечётных элементов массива: ");
+
+            x = ds.GetMultOddArrEl(array);
+
+            Console.WriteLine(x);
+
+            Console.ReadKey();
+        }
+    }
+}
