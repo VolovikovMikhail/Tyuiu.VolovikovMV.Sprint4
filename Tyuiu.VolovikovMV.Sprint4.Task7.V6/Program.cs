6b2f11acdc730281df33b4576ff59a057f0806ac
VolovikovMikhail <volovikov.1995@bk.ru>
2023-11-12 15:42:33 +0500

    Task7

---
+++ Tyuiu.VolovikovMV.Sprint4.Task7.V6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolovikovMV.Sprint4.Task7.V6.Lib;
+
+namespace Tyuiu.VolovikovMV.Sprint4.Task7.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Воловиков М. В. | АСОиУБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Воловиков Михаил Васильевич | АСОиУБ-23-3                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр. Преобразуйте ее в матрицу 3 на 4 и   *");
+            Console.WriteLine("* подсчитайте сумму нечетных чисел.                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int rows = 3;
+            int columns = 4;
+            int[,] mtrx = new int[rows, columns];
+
+            string str = "458963214789";
+            int index = 0;
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(rows, columns, str);
+
+            Console.WriteLine("Сумма нечетных элементов = " + res);
+            Console.ReadKey();
+        }
+    }
+}
